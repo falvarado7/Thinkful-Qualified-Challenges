53fc99f1bd9a249041808c884b2d38e60c00ce87
Francisco Alvarado <102932362+falvarado7@users.noreply.github.com>
2024-02-12 21:23:36 -0600

    Delete 4.4 Rendering_with_React directory

--- 4.4 Rendering_with_React/4.4.4 Import_and_Export/src/index.js
+++
@@ -1,7 +0,0 @@
-import getUrls from "./getUrls";
-
-function init() {
-  return getUrls;
-}
-
-export default init;
--- 4.4 Rendering_with_React/4.4.4 Import_and_Export/src/urls.js
+++
@@ -1,3 +0,0 @@
-export const CHEGG_URL = "https://www.chegg.com/";
-
-export const GOOGLE_URL = "https://www.google.com";
